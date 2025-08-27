527297e26ae72e03150cbc9191b589043518e906
ahmediten17 <ahmed.alzubidy@itential.com>
2025-08-26 20:39:16 -0400

    Add files via upload

--- ahmed-hello-world.py
+++ ahmed-hello-world.py
@@ -2,12 +2,14 @@
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description="ahmed gw5 demo")
-    parser.add_argument('--name', required=True, help="Name to say hello to")
-    args = parser.parse_args()
-    name = args.name
+    # parser = argparse.ArgumentParser(description="ahmed gw5 demo")
+    # parser.add_argument('--name', required=True, help="Name to say hello to")
+    # args = parser.parse_args()
+    # name = args.name
 
-    print(f"Hello, {name}")
+    # print(f"Hello, {name}")
+    
+    print("Hello World!")
 
 if __name__ == "__main__":
     main()
